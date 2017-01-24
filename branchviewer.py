c47d77e69e672afdbe7db6995a1608eba05651ce
buzzz321 <anders.olme@gmail.com>
2017-01-24 20:44:09 +0100

    added support for reading tags

--- branchviewer.py
+++ branchviewer.py
@@ -1,4 +1,8 @@
 import wx
+import subprocess
+import string
+
+REPO = '.git'
 
 class MyFrame(wx.Frame):
     """ We simply derive a new class of Frame. """
@@ -6,7 +10,7 @@ class MyFrame(wx.Frame):
         wx.Frame.__init__(self, parent, title=title, size=(200,200))
         panel = wx.Panel(self, -1, style=wx.SUNKEN_BORDER)
         
-        sizer = wx.GridBagSizer(4, 3)
+        sizer = wx.GridBagSizer()
         
         self.list_ctrl = wx.ListCtrl(panel, size=(-11,100),
                          style=wx.LC_REPORT
@@ -20,8 +24,8 @@ class MyFrame(wx.Frame):
 
         self.refreshBtn = wx.Button(panel, -1, "Refresh")
         self.deleteBtn = wx.Button(panel, -1, "Delete")
-        sizer.Add(self.refreshBtn, pos=(4, 0), span=(1,1), flag=wx.ALL|wx.EXPAND, border=5)
-        sizer.Add(self.deleteBtn, pos=(4, 3), span=(1,1), flag=wx.ALL|wx.EXPAND, border=5)
+        sizer.Add(self.refreshBtn, pos=(2, 0), span=(1,1), flag=wx.ALL|wx.EXPAND, border=5)
+        sizer.Add(self.deleteBtn, pos=(2, 3), span=(1,1), flag=wx.ALL|wx.EXPAND, border=5)
 
         for i in range(3):
             sizer.AddGrowableCol(i)
@@ -34,7 +38,27 @@ class MyFrame(wx.Frame):
         self.SetClientSize(panel.GetBestSize())
         panel.Layout()
         self.Show(True)
+
+    def add_tags(self,tags):
+        for tag in tags:
+            self.list_ctrl.Append([tag,''])
         
-app = wx.App(False)
-frame = MyFrame(None, 'Branch Deleter')
-app.MainLoop()
+def get_tags():
+     cmd = "git"
+     parameter1 = '--git-dir'
+     parameter2 = REPO
+     parameter3 = "tag"
+     try:
+         result = subprocess.check_output([cmd, parameter1, parameter2, parameter3])
+         return string.split(result, '\n')
+     except OSError as e:
+         print >>sys.stderr, "Execution failed:", e
+        
+if __name__ == '__main__':
+    tags = get_tags()
+    
+    app = wx.App(False)
+    frame = MyFrame(None, 'Branch Deleter')
+    frame.add_tags(tags)
+    
+    app.MainLoop()
